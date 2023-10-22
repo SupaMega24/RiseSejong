8657e63adf48fed5cb5357100cce7ac058fac22d
noobslearn <138974329+SupaMega24@users.noreply.github.com>
2023-10-22 23:15:00 +0900

    Add JS to mk.html for syllabus pop up window

--- script.js
+++ script.js
@@ -1,19 +1,14 @@
-const apiKey = 'YOUR_API_KEY'; // Replace 'YOUR_API_KEY' with your OpenWeatherMap API key
-        const city = 'YOUR_CITY'; // Replace 'YOUR_CITY' with the city for which you want weather information
-        const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${Sejong}&units=metric&appid=${c0bd47e4fb7cc82bb45eca26a14d40b2}`;
 
-        // Function to fetch weather data from the API
-        async function getWeatherData() {
-            try {
-                const response = await fetch(apiUrl);
-                const data = await response.json();
-                const weatherInfo = `City: ${data.name}<br>Temperature: ${data.main.temp}°C<br>Weather: ${data.weather[0].description}`;
-                document.getElementById('weather').innerHTML = weatherInfo;
-            } catch (error) {
-                console.error('Error fetching weather data:', error);
-                document.getElementById('weather').innerHTML = 'Error fetching weather data';
-            }
-        }
-
-        // Call the function to get weather data when the page loads
-        getWeatherData();
+document.getElementById("open-pdf").addEventListener("click", function(event) {
+    event.preventDefault(); // Prevent the default behavior of the link
+    
+    // Open a new popup window with specific dimensions
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+    
+    const left = (window.screen.width - width) / 2;
+    const top = (window.screen.height - height) / 2;
+    
+    window.open('https://docs.google.com/document/d/1R6sd95XCCrCIdjX3pqOPQGKnLDbJBLd3vfhDN0SNrYA/edit', "PDFPopup", "width=" + width + ",height=" + height + ",left=" + left + ",top=" + top);
+  });
+  
